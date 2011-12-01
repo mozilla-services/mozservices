a4c7825d06f77c6244f391212830d2cead13539f
Ryan Kelly <rkelly@mozilla.com>
2011-12-01 12:03:06 +1100

    Bug 707588 - Add mozsvc.user module for access to auth backends; r=telliott

---
+++ mozsvc/tests/test_user.py
@@ -0,0 +1,226 @@
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Sync Server
+#
+# The Initial Developer of the Original Code is the Mozilla Foundation.
+# Portions created by the Initial Developer are Copyright (C) 2010
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Ryan Kelly (rkelly@mozilla.com)
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
+import unittest
+
+from zope.interface import implements
+
+from pyramid.interfaces import IRequestFactory, IAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+from pyramid.request import Request
+import pyramid.testing
+
+import mozsvc.user
+
+
+class HeaderAuthenticationPolicy(object):
+    """Authentication policy taking creds from request headers."""
+
+    implements(IAuthenticationPolicy)
+
+    def authenticated_userid(self, request):
+        username = request.environ.get("HTTP_X_USERNAME")
+        if not username:
+            return None
+        password = request.environ.get("HTTP_X_PASSWORD")
+        if not password:
+            return None
+        credentials = {"username": username, "password": password}
+        if not mozsvc.user.authenticate(request, credentials, ["mail"]):
+            return None
+        request.user["x-was-ere"] = True
+        return username
+
+    def unauthenticated_userid(self, request):
+        raise RuntimeError("tests shouldn't call this")  # pragma: nocover
+
+    def effective_principals(self, request):
+        raise RuntimeError("tests shouldn't call this")  # pragma: nocover
+
+
+class UserTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.config = pyramid.testing.setUp()
+        self.config.add_settings({
+            "auth.backend": "services.user.memory.MemoryUser",
+            'cef.vendor': 'mozilla',
+            'cef.device_version': '1.3',
+            'cef.product': 'weave',
+            'cef.use': True,
+            'cef.version': 0,
+            'cef.file': 'syslog',
+        })
+        self.config.include("mozsvc")
+        self.config.include("mozsvc.user")
+        self.config.set_authorization_policy(ACLAuthorizationPolicy())
+        self.config.set_authentication_policy(HeaderAuthenticationPolicy())
+        self.auth = self.config.registry["auth"]
+        self.auth.create_user("user1", "password1", "test@mozilla.com")
+
+    def tearDown(self):
+        pyramid.testing.tearDown()
+
+    def _make_request(self, environ=None, factory=None):
+        my_environ = {}
+        my_environ["REQUEST_METHOD"] = "GET"
+        my_environ["SCRIPT_NAME"] = ""
+        my_environ["PATH_INFO"] = "/"
+        if environ is not None:
+            my_environ.update(environ)
+        if factory is None:
+            factory = self.config.registry.getUtility(IRequestFactory)
+        request = factory(my_environ)
+        request.registry = self.config.registry
+        return request
+
+    def test_auth_backend_is_loaded(self):
+        self.assertEquals(self.config.registry["auth"].__class__.__name__,
+                          "MemoryUser")
+
+    def test_authenticate(self):
+        request = self._make_request()
+        # We have no IAuthenticationPolicy, so initially req.user is False.
+        self.assertFalse(request.user)
+        # After authenticating, it will be replaced with the user object.
+        credentials = {"username": "user1", "password": "password1"}
+        mozsvc.user.authenticate(request, credentials)
+        self.assertEquals(request.user["username"], "user1")
+
+    def test_authenticate_with_normal_request_object(self):
+        request = self._make_request(factory=Request)
+        # Initially it has no user attribute.
+        self.assertRaises(AttributeError, getattr, request, "user")
+        # After authenticating, it will have the user object.
+        credentials = {"username": "user1", "password": "password1"}
+        mozsvc.user.authenticate(request, credentials)
+        self.assertEquals(request.user["username"], "user1")
+
+    def test_authenticate_with_no_backend(self):
+        del self.config.registry["auth"]
+        request = self._make_request()
+        credentials = {"username": "user1", "password": "password1"}
+        self.assertFalse(mozsvc.user.authenticate(request, credentials))
+        self.assertEquals(request.user, {})
+
+    def test_authenticate_with_repozewho_style_credentials(self):
+        request = self._make_request()
+        credentials = {"login": "user1", "password": "password1"}
+        mozsvc.user.authenticate(request, credentials)
+        self.assertEquals(request.user["username"], "user1")
+
+    def test_authenticate_with_bad_password(self):
+        request = self._make_request()
+        credentials = {"username": "user1", "password": "BAD BAD BAD"}
+        self.assertFalse(mozsvc.user.authenticate(request, credentials))
+        self.assertEquals(request.user, {})
+
+    def test_authenticate_with_unicode_password(self):
+        credentials = {"username": "user1", "password": "password1"}
+        new_password = u"password\N{GREEK SMALL LETTER ALPHA}"
+        self.auth.update_password({"username": "user1"}, credentials, new_password)
+        # Auth works with unicode password.
+        request = self._make_request()
+        credentials = {"username": "user1", "password": new_password}
+        self.assertTrue(mozsvc.user.authenticate(request, credentials))
+        # Auth works with utf-encoded password.
+        request = self._make_request()
+        credentials = {"username": "user1", "password": new_password.encode("utf8")}
+        self.assertTrue(mozsvc.user.authenticate(request, credentials))
+        # Auth fails with badly-encoded password
+        request = self._make_request()
+        credentials = {"username": "user1", "password": new_password.encode("utf16")}
+        self.assertFalse(mozsvc.user.authenticate(request, credentials))
+
+    def test_authenticate_with_unknown_username(self):
+        request = self._make_request()
+        credentials = {"username": "user2", "password": "password1"}
+        self.assertFalse(mozsvc.user.authenticate(request, credentials))
+        self.assertEquals(request.user, {})
+
+    def test_authenticate_with_no_username(self):
+        request = self._make_request()
+        credentials = {"usernme": "user2", "password": "password1"}
+        self.assertFalse(mozsvc.user.authenticate(request, credentials))
+        self.assertEquals(request.user, {})
+
+    def test_includeme_with_bad_backend(self):
+        config = pyramid.testing.setUp()
+        self.config.add_settings({
+            "auth.backend": "this.does.not.exist",
+        })
+        config.include("mozsvc.user")
+        self.assertEquals(config.registry["auth"], None)
+        request = self._make_request()
+        self.assertEquals(request.user, {})
+
+    def test_req_user_success(self):
+        request = self._make_request({
+                    "HTTP_X_USERNAME": "user1",
+                    "HTTP_X_PASSWORD": "password1",
+                  })
+        self.assertEquals(request.user["username"], "user1")
+        self.assertEquals(request.user.get("password"), None)
+        self.assertEquals(request.user["mail"], "test@mozilla.com")
+        self.assertEquals(request.user["x-was-ere"], True)
+
+    def test_req_user_bad_password(self):
+        request = self._make_request({
+                    "HTTP_X_USERNAME": "user1",
+                    "HTTP_X_PASSWORD": "random_guess",
+                  })
+        self.assertFalse(request.user)
+
+    def test_req_user_no_username(self):
+        request = self._make_request({
+                    "HTTP_X_PASSWORD": "password1",
+                  })
+        self.assertFalse(request.user)
+
+    def test_req_user_no_password(self):
+        request = self._make_request({
+                    "HTTP_X_USERNAME": "user1",
+                  })
+        self.assertFalse(request.user)
+
+    def test_req_user_gets_info_from_repoze(self):
+        request = self._make_request({
+                    "HTTP_X_USERNAME": "user1",
+                    "HTTP_X_PASSWORD": "password1",
+                    "repoze.who.identity": {"repoze-was-ere": True},
+                  })
+        self.assertEquals(request.user["username"], "user1")
+        self.assertEquals(request.user["repoze-was-ere"], True)
---
+++ mozsvc/user.py
@@ -0,0 +1,160 @@
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Sync Server
+#
+# The Initial Developer of the Original Code is the Mozilla Foundation.
+# Portions created by the Initial Developer are Copyright (C) 2011
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Ryan Kelly (rkelly@mozilla.com)
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+"""
+
+Utilities for authentication via a generic user backend.
+
+Include mozsvc.user into your pyramid config to get the following niceties:
+
+    * user database backend loaded as a plugin from "auth" config section
+    * user database backend available as request.registry["auth"]
+    * authenticated user's data as a dict at request.user
+
+You can also use the function mozsvc.user.authenticate(req, creds) as a
+shortcut for authenticating against the configured backend.
+
+"""
+
+from pyramid.decorator import reify
+from pyramid.request import Request
+from pyramid.security import authenticated_userid
+
+from cef import log_cef, AUTH_FAILURE
+
+from mozsvc.plugin import load_and_register
+
+import logging
+logger = logging.getLogger("mozsvc.user")
+
+
+class RequestWithUser(Request):
+    """Request object that exposes the current user as "request.user".
+
+    This is a custom Pyramid Request class that does user authentication
+    on demand.  When you access the "user" attribute it will automatically
+    search for credentials in the request, authenticate them, create a dict
+    of user data and return it.
+
+    If there are no credentials in the request then the "user" attribute will
+    be an empty dict.  If the call to authenticated_userid() raises an error
+    it will *not* be intercepted, you will receive it as an error from the
+    attribute access attempt.
+    """
+
+    @reify
+    def user(self):
+        # Create an empty user dict and store it on the request.
+        # This makes it available for authenticated_userid to populate.
+        user = self.__dict__["user"] = {}
+        # Do the authentication through the standard pyramid interface.
+        username = authenticated_userid(self)
+        if username is not None:
+            user["username"] = username
+            # Suck in extra information from likely sources.
+            # Currently this just looks for a repoze.who identity dict.
+            if "repoze.who.identity" in self.environ:
+                user.update(self.environ["repoze.who.identity"])
+        return user
+
+
+def authenticate(request, credentials, attrs=()):
+    """Authenticate a dict of credentials against the configured user backend.
+
+    This is a handy callback that you can use to check a dict of credentials
+    against the configured auth backend.  It will accept credentials from any
+    of the auth schemes supported by the backend.  If the authentication is
+    successful it will update request.user with the user object loaded from
+    the backend.
+    """
+    # Use whatever auth backend has been configured.
+    auth = request.registry.get("auth")
+    if auth is None:
+        return False
+
+    # Update an existing user object if one exists on the request.
+    user = getattr(request, "user", None)
+    if user is None:
+        user = {}
+
+    # Ensure that we have credentials["username"] for use by the backend.
+    # Some repoze.who plugins like to use "login" instead of "username".
+    # In the end, we must have credentials["username"].
+    if "username" not in credentials:
+        if "login" in credentials:
+            credentials["username"] = credentials.pop("login")
+        else:
+            log_cef("Authentication attemped without username", 5,
+                    request.environ, request.registry.settings,
+                    "", signature=AUTH_FAILURE)
+            return False
+
+    # Normalize the password, if any, to be unicode.
+    if "password" in credentials and not isinstance(credentials["password"], unicode):
+        try:
+            credentials["password"] = credentials["password"].decode("utf8")
+        except UnicodeDecodeError:
+            return None
+
+    # Authenticate against the configured backend.
+    if not auth.authenticate_user(user, credentials, attrs):
+        log_cef("Authentication Failed", 5,
+                request.environ, request.registry.settings,
+                credentials["username"],
+                signature=AUTH_FAILURE)
+        return False
+
+    # Store the user dict on the request, and return it for conveience.
+    if getattr(request, "user", None) is None:
+        request.user = user
+    return user
+
+
+def includeme(config):
+    """Include mozsvc user-handling into a pyramid config.
+
+    This function will set up user-handling via the mozsvc.user system.
+    Things configured include:
+
+        * using RequestWithUser as the request object factory
+        * loading a user database backend from config section "auth"
+
+    """
+    config.set_request_factory(RequestWithUser)
+    try:
+        config.registry["auth"] = load_and_register("auth", config)
+    except Exception, e:
+        logger.exception("Unable to load auth backend. Problem? %s" % e)
+        config.registry["auth"] = None
