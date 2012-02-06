969227cc82672a2b646f1988c8b103e5727d1c92
Ryan Kelly <rkelly@mozilla.com>
2012-02-06 15:37:13 +1100

    Fix typo in function name.

--- mozsvc/views.py
+++ mozsvc/views.py
@@ -1,11 +1,11 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
-""" Cornice default views/.
+""" Cornice default views.
 """
 from pyramid.view import view_config
 
 
 @view_config(route_name='heartbeat', renderer='string')
-def hearbeat(request):
+def heartbeat(request):
     return 'OK'
