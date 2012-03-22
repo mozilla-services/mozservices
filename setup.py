f0141987a206ff47a028537c152b7bc41c8c94a2
Tarek Ziade <tarek@ziade.org>
2012-03-22 23:28:23 +0100

    added cornice in the deps

--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
 requires = ['pyramid', 'pyramid_debugtoolbar', 'simplejson', 'cef',
             'unittest2', 'WebTest', 'WSGIProxy', 'repoze.who',
             'repoze.who.plugins.macauth', 'pyramid_whoauth',
-            'tokenlib', 'macauthlib', 'metlog-py[zeromqpub]>=0.8.2']
+            'tokenlib', 'macauthlib', 'metlog-py[zeromqpub]>=0.8.2',
+            'cornice']
+
 
 setup(name='mozsvc',
       version='0.1',
